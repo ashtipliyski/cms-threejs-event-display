6dbbbd5eff44a8bbfc3175d72bdc03c6ed374ac0
Antoni Shtipliyski <a.shtipliyski@nullone.com>
2015-08-13 12:08:52 +0100

    Added raw CSV file with geometry, created Python script for processing and added the full geometry to the application'

---
+++ geometry/generate_geometry.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python2
+
+"""
+This version of the code generates all modules arranged 
+tangential to the barrel but at different radial separation
+"""
+
+import matplotlib.pyplot as plt;
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np;
+
+
+fig_size = 7;
+points_num = 20;
+radius = 100;   # radius of barrel
+length = 40; # module length
+width = 5  # module thickness
+thickness = 8;
+z_spacing = 2;
+module_rotation = 0 # alpha, rotation of modules in degrees
+module_rotation = module_rotation * np.pi/180;
+
+
+
+fig = plt.figure(figsize=(fig_size, fig_size))
+ax = fig.add_subplot(111, projection='3d')
+plt.grid();
+
+def generate_module_pos(n, r, w, l, a):
+    indentation_r = 0.08
+
+    modules = [{}] * n;
+    
+    for i in range(0, n):
+        if i % 2 == 0:
+            indent = True;
+            extra_r = 0;
+        else: 
+            indent = False;
+            extra_r = indentation_r
+        
+        theta = 2 * np.pi * i/n;
+        x = r * np.cos(theta);
+        y = r * np.sin(theta);
+    
+        theta_r = np.pi/2 - theta; # rotated theta
+        cos_th = np.cos(theta)
+        sin_th = np.sin(theta)
+    
+        r_x = r * (1 + extra_r) * cos_th    
+        r_y = r * (1 + extra_r) * sin_th
+    
+        # r hat cartesian components
+        r_hat_x = np.cos(a) * cos_th - np.sin(a) * sin_th
+        r_hat_y = np.sin(a) * cos_th + np.cos(a) * sin_th
+    
+        # theta hat cartesian components
+        t_hat_x = -np.cos(a) * sin_th - np.sin(a) * cos_th
+        t_hat_y = -np.sin(a) * sin_th + np.cos(a) * cos_th
+    
+        # equations accounting for module rotation
+        x_vect = [
+            r_x + 0.5 * l * t_hat_x - 0.5 * w * r_hat_x, # r1
+            r_x + 0.5 * l * t_hat_x + 0.5 * w * r_hat_x,  # r2
+            r_x - 0.5 * l * t_hat_x + 0.5 * w * r_hat_x, # r3
+            r_x - 0.5 * l * t_hat_x - 0.5 * w * r_hat_x # r4
+        ]
+    
+        y_vect = [
+            r_y + 0.5 * l * t_hat_y - 0.5 * w * r_hat_y, # r1
+            r_y + 0.5 * l * t_hat_y + 0.5 * w * r_hat_y,  # r2
+            r_y - 0.5 * l * t_hat_y + 0.5 * w * r_hat_y, # r3
+            r_y - 0.5 * l * t_hat_y - 0.5 * w * r_hat_y # r4
+        ]
+
+        modules[i] = {"x": x_vect, "y": y_vect};
+        
+    return modules;
+
+gen_mods= generate_module_pos(points_num, radius, width, length, module_rotation);
+
+modules_num = 20;
+z = 0
+
+# blocks = [{}] * modules_num;
+blocks = [];
+for z in range(0, modules_num):
+    for j in range(0, len(gen_mods)):
+
+
+        block = {
+            "x": [
+                gen_mods[j]['x'][0], 
+                gen_mods[j]['x'][1], 
+                gen_mods[j]['x'][2],
+                gen_mods[j]['x'][3], 
+                gen_mods[j]['x'][0],  
+                gen_mods[j]['x'][1], 
+                gen_mods[j]['x'][2],
+                gen_mods[j]['x'][3]  
+                
+            ], #gen_mods[j]['x'] + gen_mods[j]['x'],
+            "y": [
+                gen_mods[j]['y'][0], 
+                gen_mods[j]['y'][1], 
+                gen_mods[j]['y'][2],
+                gen_mods[j]['y'][3], 
+                gen_mods[j]['y'][0],  
+                gen_mods[j]['y'][1], 
+                gen_mods[j]['y'][2],
+                gen_mods[j]['y'][3]
+            ], #gen_mods[j]['y'] + gen_mods[j]['y'],
+            "z": [
+                z * 20,
+                z * 20,
+                z * 20,
+                z * 20,
+                z * 20 + 18,
+                z * 20 + 18,
+                z * 20 + 18,
+                z * 20 + 18
+            ]
+        };
+
+        blocks.append(block);
+
+        # print "processing block %i / %i." % ( (z+1) *(j+1), modules_num * len(gen_mods));
+        # print "\r"
+    
+        # front points
+        plt.plot(gen_mods[j]['x'], gen_mods[j]['y'], [z * 20] * 4,'r-');
+
+        # rear points
+        plt.plot(gen_mods[j]['x'], gen_mods[j]['y'], [z * 20 + 18] * 4,'r-');
+
+# print the module itself
+# plt.plot(x_vect, y_vect, [0, 0, 0, 0, 0], 'r-')
+
+# plt.scatter(points[0], points[1]);
+# plt.show();
+# plt.savefig("geometry.png");
+
+import json
+with open('barrel.json', 'w') as outputfile:
+    json.dump(blocks, outputfile)
+    print "Geometry successfuly exported"
+    
---
+++ geometry/process_geometry.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python2
+
+"""
+Description of RAW data format:
+
+Layer/Disk Identifier (B=barrel, E=endcap)
+PS or 2S module (PS=1,2S=0)
+Module ID
+Sensor ID (top/bottom sensor)
+3D Cyclindrical Polar Coordinate Vector[x8] (r, phi, z)
+
+"""
+
+import sys, getopt;
+
+argv = sys.argv[1:]
+
+inputfile = ''
+outputfile = ''
+verbose = False
+
+def display_help():
+    print "process_geometry -i <inputfile> -o <outputfile> [-v -h]"
+
+try:
+    opts, args = getopt.getopt(argv, "hi:o:v", ["ifile=", "ofile="])
+except getopt.GetoptError:
+    display_help()
+    sys.exit(2)
+
+for opt, arg in opts:
+    if opt == "-h":
+        display_help()
+        sys.exit()
+    elif opt in ("-i", "--ifile"):
+        inputfile = arg
+    elif opt in ("-o", "--ofile"):
+        outputfile = arg
+    elif opt in ("-v", "--verbose"):
+        verbose = True
+
+if inputfile == '':
+    print "Input file must be specified, exiting."
+    display_help()
+    sys.exit(2)
+
+if outputfile == '':
+    print "Output file must be specified, exiting."
+    display_help()
+    sys.exit(2)
+        
+import csv;
+import numpy as np;
+
+# raw_filename = "raw/geometry.csv"
+raw_filename = inputfile
+
+with open(raw_filename, "rb") as file:
+    reader = csv.reader(file);
+    
+    data = []
+
+    if verbose:
+        print "Processing data"
+
+    # prev_obj = {};
+    prev_r_vect = []
+    prev_theta_vect = []
+    prev_mod_id = 0
+    
+    for row in reader:
+        identifier = row[0]
+        mod_type = row[1]
+        mod_id   = row[2]
+        sensor_id = row[3]
+        r_vect = [
+            float(row[4]), 
+            float(row[7]),
+            float(row[10]),
+            float(row[13]),
+            float(row[16]),
+            float(row[19]),
+            float(row[22]),
+            float(row[25])
+        ];
+
+        theta_vect = [
+            float(row[5]),
+            float(row[8]),
+            float(row[11]),
+            float(row[14]),
+            float(row[17]),
+            float(row[20]),
+            float(row[23]),
+            float(row[26])
+        ];
+
+        
+
+        # add condition here to modify obj in a way that combines the
+        # positions of two previous modules
+        
+        if mod_id == prev_mod_id:
+            x_vect = [
+            ];
+            y_vect = [
+            ];
+            
+            for i in range(0, len(r_vect) / 2):
+                x_vect.append(r_vect[i] * np.cos(theta_vect[i]));
+                y_vect.append(r_vect[i] * np.sin(theta_vect[i]));
+                
+            for i in range(len(r_vect) / 2, len(r_vect)):
+                x_vect.append(prev_r_vect[i] * np.cos(prev_theta_vect[i]));
+                y_vect.append(prev_r_vect[i] * np.sin(prev_theta_vect[i]));
+
+            z_vect = [
+                row[6],
+                row[9],
+                row[12],
+                row[15],
+                row[18],
+                row[21],
+                row[24],
+                row[27]
+            ];
+            
+            obj = {
+            "identifier": identifier,
+                "mod_type": mod_type,
+                "mod_id": mod_id,
+                "sensor_id": sensor_id,
+                "x": x_vect,
+                "y": y_vect,
+                "z": z_vect
+            }
+        
+            data.append(obj)
+
+        prev_mod_id = mod_id;
+        prev_r_vect = r_vect
+        prev_theta_vect = theta_vect
+#    print data
+
+if verbose:
+    print "Exporting data";
+
+import json
+with open(outputfile, 'w') as outputfile:
+    json.dump(data, outputfile)
+    
+    if verbose:
+        print "Geometry successfuly exported"
