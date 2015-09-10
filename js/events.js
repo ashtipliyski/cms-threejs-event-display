7c1559aa5c8a42194a7ad9f935e8c4d95eb12a86
Antoni Shtipliyski <a.shtipliyski@nullone.com>
2015-09-10 12:29:44 +0100

    Implemented regexp extraction of links from events page

--- js/events.js
+++ js/events.js
@@ -11,37 +11,28 @@ $(function(){
             {
                 dataType: "html",
                 success: function (data) {
-                    var parsed_html = $.parseHTML(data);
 
-                    var files_table = parsed_html[9];
-                    var file_trs = files_table.children[0].children;
+                    var links_list = data.match(
+                            /<a href="([/a-z_0-9\-]+\.js)">[a-z_0-9\-]+\.js<\/a>/g
+                    );
 
-                    for (var i in file_trs)
-                    {
-                        if (i != 0 && file_trs.hasOwnProperty(i)) {
-                            var a_str = file_trs[i].children[2].innerHTML;
+                    for (var i in links_list)
+                    {           
+                        var result = links_list[i].match(/href="([^"]*")/g)[0];
+                        var file = result.substring(
+                            6, result.length - 1
+                        );
 
-                            var el_flags = file_trs[i].children[0].children[0]; 
-                            var el_type = el_flags.innerHTML.substr(1, 1);
+                        var extension = result.substr(
+                            result.length - 3,
+                            2
+                        );
 
-                            if (el_type == '-') {
-                                
-                                var result = a_str.match(/href="([^"]*")/g)[0];
-                                var file = result.substring(
-                                    6, result.length - 1
-                                );
+                        console.log(file);
 
-                                var extension = result.substr(
-                                    result.length - 3,
-                                    2
-                                );
-
-                                if (extension === "js") {
-                                    event_files.push(file);
-                                }
-                                
-                            }
-                        }
+                        if (extension === "js") {
+                            event_files.push(file);
+                        }                        
                     }
 
                     document.generate_event_files_list(event_files);
